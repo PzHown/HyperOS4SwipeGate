380181ee5d9579517f6800de22e4287eadd00a62
PzHown <1263097055@qq.com>
2026-08-27 17:11:03 +0800

    build: add HyperCeiler MIUIX package repository

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,11 +6,22 @@ pluginManagement {
     }
 }
 
+val gprUser = providers.gradleProperty("gpr.user").orNull
+    ?: System.getenv("GITHUB_ACTOR")
+val gprKey = providers.gradleProperty("gpr.key").orNull
+    ?: System.getenv("GITHUB_TOKEN")
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven("https://maven.pkg.github.com/ReChronoRain/HyperCeiler") {
+            credentials {
+                username = gprUser ?: "x-access-token"
+                password = gprKey ?: ""
+            }
+        }
         maven("https://api.xposed.info/")
     }
 }
