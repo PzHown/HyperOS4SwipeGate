7b61ed4356eaa6e9e8045df00dafaea69021f921
PzHown <1263097055@qq.com>
2026-08-30 16:59:48 +0800

    dev: bump version to 0.7.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "io.github.pzhown.hyperos4swipegate"
         minSdk = 33
         targetSdk = 37
-        versionCode = 33
-        versionName = "0.7.1"
+        versionCode = 34
+        versionName = "0.7.2"
 
         ndk {
             abiFilters += "arm64-v8a"
