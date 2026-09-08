0460e644a6f498ddc04f6db45ed59a87eb49b416
PzHown <1263097055@qq.com>
2026-09-08 21:33:58 +0800

    test: add compile-only app stubs

---
+++ tests/stubs/java/io/github/pzhown/hyperos4swipegate/XposedServiceBridge.java
@@ -0,0 +1,6 @@
+package io.github.pzhown.hyperos4swipegate;
+import android.content.Context;
+public final class XposedServiceBridge {
+ public static void applyThresholdDp(Context context,int value) {}
+ public static void applyLogLevel(Context context,int value) {}
+}
